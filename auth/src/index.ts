e17b17f12ef79a919a5894e64a0fa42242eda2ce
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-02 16:19:20 +0300

    initial commit

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
-import 'express-async-errors'
+import 'express-async-errors';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentuserRouter } from './routes/current-user';
 import { signupRouter } from './routes/signup';
@@ -9,7 +11,17 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+
+app.set('trust proxy', true);
+
 app.use(express.json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 const PORT = 3000;
 
@@ -21,6 +33,22 @@ app.use(signoutRouter);
 app.all('*', () => {
   throw new NotFoundError();
 });
+
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+start();
