db97db3ffe1cd1e4544bb6ff808c6603f049eb22
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-02 16:20:15 +0300

    initial commit

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const currentuserRouter = express.Router();
 
-currentuserRouter.get('/api/users/currentuser', (request, response) => {
-  response.send({});
-});
+currentuserRouter.get(
+  '/api/users/currentuser',
+  currentUser,
+  requireAuth,
+  (request, response) => {
+    response.send({ currentUser: request.currentUser || null });
+  }
+);
 
 export { currentuserRouter };
