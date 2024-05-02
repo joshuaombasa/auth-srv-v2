e9952a8c16b62393d05cd46b09eca8ada9d91b6b
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-02 16:20:50 +0300

    initial commit

---
+++ auth/src/middlewares/require-auth.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { NotAuthorizedError } from '../errors/not-authorized-error';
+
+const requireAuth = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  if (!request.currentUser) {
+    throw new NotAuthorizedError();
+  }
+
+  next();
+};
+
+
+export {requireAuth}
