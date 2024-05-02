828182abffbe7c586350efff2b6a55c75b8a3405
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-02 16:19:59 +0300

    initial commit

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,6 +3,8 @@ import express from 'express';
 const signoutRouter = express.Router();
 
 signoutRouter.post('/api/users/signout', (request, response) => {
+  request.session = null;
+
   response.send({});
 });
 
